0e6a347b2a1755568856c0899ee18e35336f2a4a
Alan Koger <akoger@westcarygroup.com>
2018-12-07 10:58:25 -0500

    Log 'this' value from input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button type="submit">Visit Store -></button>
-      </form>
-    );
-  }
+    constructor() {
+        super();
+        console.log("gonna create component!");
+        this.goToStore = this.goToStore.bind(this);
+    }
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. Stop form from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        console.log(this.myInput.value.value);
+        // 3. Change the page to /store/whatever-they-entered
+    };
+    render() {
+        return (
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2>Please Enter A Store</h2>
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                />
+                <button type="submit">Visit Store -></button>
+            </form>
+        );
+    }
 }
 
 export default StorePicker;
